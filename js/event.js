82fccf1387eb973a8bb059edca503fd5f57ca25c
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-10 17:48:53 +0000

    ok

---
+++ js/event.js
@@ -0,0 +1,12 @@
+
+function like() {
+    let val = 1;
+    if (val > 0) {
+        let value = 1;
+        value++;
+        const likeValue = document.getElementById('unlimited-like');
+        likeValue.innerText = value;
+    }
+
+
+}
