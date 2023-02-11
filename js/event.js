41b1f30939dac9a4a72d40314d1a13c27c19eed8
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-11 18:06:58 +0000

    ok

--- js/event.js
+++ js/event.js
@@ -14,3 +14,14 @@ function like() {
 document.getElementById('blue-color').addEventListener('click',function(){
     document.body.style.backgroundColor='blue';
 })
+
+document.getElementById('post-btn').addEventListener('click', function(){
+    const comment = document.getElementById('write-box');
+    commentValue = comment.value;
+    const cmntbox = document.getElementById('comment-box');
+    const p = document.createElement('p');
+    p.innerText = commentValue;
+    cmntbox.appendChild(p);
+    comment.value = '';
+
+})
